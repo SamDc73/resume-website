3ca586703089634109ab33f6b90a63c122b2c093
Husam Alshehadat <65294070+SamDc73@users.noreply.github.com>
2022-09-19 16:21:37 -0700

    Add files via upload
    Initial Commit

---
+++ scripts/main.js
@@ -0,0 +1,136 @@
+(function() {
+  "use strict";
+
+  window.addEventListener('load', () => {
+    on_page_load()
+  });
+
+  /**
+   * Function gets called when page is loaded.
+   */
+  function on_page_load() {
+    // Initialize On-scroll Animations
+    AOS.init({
+      anchorPlacement: 'top-left',
+      duration: 600,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+      disable: 'mobile'
+    });
+  }
+
+  /**
+   * Navbar effects and scrolltop buttons upon scrolling
+   */
+  const navbar = document.getElementById('header-nav')
+  var body = document.getElementsByTagName("body")[0]
+  const scrollTop = document.getElementById('scrolltop')
+  window.onscroll = () => {
+    if (window.scrollY > 0) {
+      navbar.classList.add('fixed-top', 'shadow-sm')
+      body.style.paddingTop = navbar.offsetHeight + "px"
+      scrollTop.style.visibility = "visible";
+      scrollTop.style.opacity = 1;
+    } else {
+      navbar.classList.remove('fixed-top', 'shadow-sm')
+      body.style.paddingTop = "0px"
+      scrollTop.style.visibility = "hidden";
+      scrollTop.style.opacity = 0;
+    }
+  };
+
+  /**
+   * Masonry Grid
+   */
+  var elem = document.querySelector('.grid');
+  if(elem) {
+    imagesLoaded(elem, function() {
+      new Masonry(elem, {
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        horizontalOrder: true
+      });
+    })
+  }
+
+  /**
+   * Big Picture Popup for images and videos
+   */
+   document.querySelectorAll("[data-bigpicture]").forEach((function(e) {
+     e.addEventListener("click", (function(t){
+       t.preventDefault();
+       const data =JSON.parse(e.dataset.bigpicture)
+       BigPicture({
+        el: t.target,
+        ...data
+      })
+     })
+    )
+  }))
+
+  /**
+   * Big Picture Popup for Photo Gallary
+   */
+   document.querySelectorAll(".bp-gallery a").forEach((function(e) {
+    var caption = e.querySelector('figcaption')
+    var img = e.querySelector('img')
+    // set the link present on the item to the caption in full view
+    img.dataset.caption = '<a class="link-light" target="_blank" href="' + e.href + '">' + caption.innerHTML + '</a>';
+    window.console.log(caption, img)
+     e.addEventListener("click", (function(t){
+       t.preventDefault();
+       BigPicture({
+        el: t.target,
+        gallery: '.bp-gallery',
+      })
+     })
+    )
+  }))
+
+  // Add your javascript here
+  // Typing effect 
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Pythonist", "Software Engineer", "Data Analyist"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+
+})();
